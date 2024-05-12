90d4f4f2f9fc1a74cba455c20fee6303fe1463a0
mayday <maydaythecoder@gmail.com>
2024-05-12 18:53:00 -0400

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Java and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/maydaythecoder" target="_blank">Github</a>!`,
+  "Graphic design": `This is my first graphic design. I hung it on the wall because I'm proud of it. It was my introduction to computers but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Technical Tutorials. `,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@Codebagel" target="_blank">Codebagel</a>,
+  <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a> (sometimes) and <a href="https://www.youtube.com/@KevinPowell" target="_blank">KevinPowell</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/maydaythecoder/resume/blob/main/Latest%20Resume.pdf" target="_blank">Check it out?</a>
+  Contact me at muhyadin.mohamed.003@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in French (I don't speak French btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit muhyadin's portfolio, just close the tab.`,
+};
